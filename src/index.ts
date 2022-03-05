c9b209e5bf6d2a4921cbbcd995c8d4c261e27a95
fabrizio <fabrizio@ubimate.com>
2022-03-06 00:26:54 +0100

    cleanup

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import helloWorld from "./samples/HelloWorld";
-import staticList from "./samples/StaticList";
-import dynamicList from "./samples/DynamicList";
-
-export default function () {
-	// helloWorld();
-	staticList();
-	// dynamicList();
-};
---
+++ test/dummy.test.ts
@@ -0,0 +1,8 @@
+import { assert } from "chai";
+
+describe('exprima/codegen', function () {
+
+	it("should succeed", () => {
+	});
+
+});
